e71818c7f8297d8e709e27f19b58c814b45a191a
Kyle Kibet <kaelkibet@gmail.com>
2022-12-09 00:13:34 +0300

    Outputs:-"Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char s[] = "\"Programming is like building a multilingual puzzle";
+	puts(s);
+	return (0);
+}

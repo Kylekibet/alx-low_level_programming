1c437c8e03888cd904d722e0aa37335cad1b369a
Kyle Kibet <kaelkibet@gmail.com>
2022-12-09 15:19:21 +0300

    added to check if less than

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	else if (n == 0)
 		printf("Last digit of %d is 0 and is %d\n", n, n % 10);
-	else
+	else if (n < 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	return (0);
 }

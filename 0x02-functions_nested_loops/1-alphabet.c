2c76498a6ef0e79bd10f8ca11dde5e824c911e28
Kyle Kibet <kaelkibet@gmail.com>
2022-12-14 11:26:05 +0300

    print alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x < 123; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}

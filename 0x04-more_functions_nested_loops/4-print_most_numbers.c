d016678fe188892eb7d9198ea766205f8d404ae9
Kyle Kibet <kaelkibet@gmail.com>
2022-12-16 01:42:52 +0300

    print all numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print from 0 - 9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		if (x != 50 && x != 52)
+			_putchar(x);
+	}
+	_putchar('\n');
+}

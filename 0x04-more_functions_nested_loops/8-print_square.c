c9687d07cb6336ad58952f6d07f5bcfae283296c
Kyle Kibet <kaelkibet@gmail.com>
2022-12-17 02:57:46 +0300

    print square made of #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of square to print
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}

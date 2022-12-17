ad9814ffe75dccba9b133e4964d0480936ee7d7d
Kyle Kibet <kaelkibet@gmail.com>
2022-12-17 03:45:57 +0300

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,9 +12,9 @@ int main(void)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz");
-		else if ( x % 3 == 0)
+		else if (x % 3 == 0)
 			printf("Fizz");
-		else if ( x % 5 == 0)
+		else if (x % 5 == 0)
 			printf("Buzz");
 		else
 			printf("%d", x);

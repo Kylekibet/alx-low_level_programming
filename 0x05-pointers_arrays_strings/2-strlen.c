69d0dba1998ccf086b984c3cff66264dfc47c0e5
Kyle Kibet <kaelkibet@gmail.com>
2022-12-20 04:24:30 +0300

    get lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - get lenth of a string
+ * @s: string to get lenth
+ * Return: Lenght of string
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while(s[x] != '\0')
+		x++;
+	return (x);
+}

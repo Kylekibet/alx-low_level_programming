8f8f39333f7b5bb435a2585c91e8a95591a480ff
kylekibet <kaelkibet@gmail.com>
2023-01-17 14:19:30 +0300

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(arr) * size);
+	arr = (char *) malloc(sizeof(arr) * size);
 	if (arr == NULL)
 		return (NULL);
 

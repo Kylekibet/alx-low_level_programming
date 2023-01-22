826b5d7a85e6e2023afbf1b9d95cf91c1bcce606
kylekibet <kaelkibet@gmail.com>
2023-01-23 00:19:32 +0300

    dog details

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog details
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

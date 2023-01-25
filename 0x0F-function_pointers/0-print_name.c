e1111e09c7a09e843abc4ddd0a9b9acd0d87881d
Kyle Kibet <kaelkibet@gmail.com>
2023-01-25 09:07:59 +0300

    print modified name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - pass name to a function modifier
+ * @f: function to modify name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

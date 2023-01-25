21bd02fe6a8269b725a41bf05fadaaa1b1f60a1d
Kyle Kibet <kaelkibet@gmail.com>
2023-01-25 15:50:28 +0300

    free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,12 +12,24 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	if (name == NULL || age < 0 || owner == NULL)
+		return(NULL);
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
+		return (NULL);
+
+	d->name = malloc(sizeof(char) *(strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	d->owner = malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->owner == NULL)
 	{
 		free(d->name);
-		free(d->owner);
 		free(d);
 		return (NULL);
 	}

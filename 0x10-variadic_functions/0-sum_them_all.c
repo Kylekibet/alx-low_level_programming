8fb99e1b503c6ddd0101a81e048802883e500a14
Kyle Kibet <kaelkibet@gmail.com>
2023-01-25 07:10:57 +0300

    sum all the numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds all number provided as arguments
+ * except the first argument
+ * @n: number of arguments provided
+ *
+ * Return: the total sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i, sum = 0;
+
+	va_list ap;
+
+	va_start(ap, n);
+
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}

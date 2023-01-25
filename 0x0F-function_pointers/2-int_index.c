e9dc4ae19fbb108b34cfbf5c06e10215ab19d0ea
Kyle Kibet <kaelkibet@gmail.com>
2023-01-25 10:47:20 +0300

    added space between NULL and ==

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,7 +13,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp ==NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)

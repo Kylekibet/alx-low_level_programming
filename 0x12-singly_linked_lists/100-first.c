83027494cad27b5e8d642dfe264238acc07a0b30
Kyle Kibet <kaelkibet@gmail.com>
2023-01-31 10:49:13 +0300

    premain

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@
 
 void __attribute__((constructor)) premain()
 {
-	printf("You're beat! and yet, you must allow,\
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }

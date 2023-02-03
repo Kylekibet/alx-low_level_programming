63ff39dffe09801e53c90d03d1942ef4bac895a3
Kyle Kibet <kaelkibet@gmail.com>
2023-02-03 22:06:20 +0300

    update

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+  * delete_nodeint_at_index - delete a node at a given index
+  * @head: list to remove node
+  * @index: index of node to remove
+  *
+  * Return: 1 if successful and -1 if failed
+  */
+
+int delete_nodint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t temp;
+	if (head == NULL)
+		return (NULL);
+
+	return (temp);
+}
+}

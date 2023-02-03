2350792d20def3a292479cf2756e70c23155c218
Kyle Kibet <kaelkibet@gmail.com>
2023-02-03 20:55:55 +0300

    new update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (index < idx)
 	{
+		if (temp == NULL || new->next == NULL)
+			return (NULL);
 		index++;
 		temp = temp->next;
 	}

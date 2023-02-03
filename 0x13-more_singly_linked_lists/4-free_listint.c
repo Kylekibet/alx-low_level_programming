ed5d06421e43857a61e4f49ad9b962f2902161cf
Kyle Kibet <kaelkibet@gmail.com>
2023-02-03 12:08:42 +0300

    free some space

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * free_listint - releases spaces created for listint_t
+  * @head: linked list to create space
+  */
+
+void free_listint(listint_t *head)
+{
+	listint_t  *temp;
+
+	while(head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}

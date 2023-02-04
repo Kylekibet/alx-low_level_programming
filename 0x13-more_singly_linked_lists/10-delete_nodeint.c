38cf8298edca72bfabdf74cae7a13b2096c604bd
Kyle Kibet <kaelkibet@gmail.com>
2023-02-04 15:02:16 +0300

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,12 +8,28 @@
   * Return: 1 if successful and -1 if failed
   */
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index __attribute__((unused)))
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t temp __attribute__((unused));
+	listint_t *temp = *head, *current;
+	unsigned int x;
 
-	if (head == NULL)
+	if (temp == NULL)
 		return (-1);
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+	for (x = 0; x < (index - 1); x++)
+	{
+		if (temp->next == NULL)
+			return -1;
+		temp = temp->next;
+	}
 
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
 	return (1);
 }

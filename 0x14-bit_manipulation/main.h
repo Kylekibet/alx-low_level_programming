a88a4496d30f2042f0900ac51d3ba0fd7900fc57
Kyle Kibet <kaelkibet@gmail.com>
2023-02-07 17:38:34 +0300

    prit the binary representation of a number

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,10 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <math.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 
 #endif
